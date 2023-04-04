5a719f690fdb0abbeb1ffe2031530769a9cfa8fa
soumany <soumany06@gmail.com>
2023-04-04 23:16:55 +0700

    program

---
+++ Generic_Stack/Generic_Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic; //Generic stack definition is included into our project space
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+        }
+    }
+}
