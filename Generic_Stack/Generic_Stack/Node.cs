4d27e9af7966abe88b185629d66b6a44b3fdc6f5
soumany <soumany06@gmail.com>
2023-04-04 23:16:41 +0700

    class node

---
+++ Generic_Stack/Generic_Stack/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Stack
+{
+    internal class Node //user defined type
+    {
+        public int idata;
+        //any other thing
+    }
+}
